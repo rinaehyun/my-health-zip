0974af25f865960d2884ab8d515f1930c7b3b8c5
Rinae Hyun <rnhyun.dream@gmail.com>
2024-12-05 14:16:03 +0100

    Write integration tests for Saving BP data with exception and Deleting BP Data / add @DirtiesContext to each test

--- backend/src/test/java/com/myhealthzip/backend/bloodpressure/controller/BloodPressureIntegrationTest.java
+++ backend/src/test/java/com/myhealthzip/backend/bloodpressure/controller/BloodPressureIntegrationTest.java
@@ -9,14 +9,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneId;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -37,6 +37,7 @@ class BloodPressureIntegrationTest {
     }
 
     @Test
+    @DirtiesContext
     void getBloodPressuresTest_whenDBIsEmpty_thenReturnEmptyList() throws Exception {
         // GIVEN
         // WHEN
@@ -47,6 +48,7 @@ class BloodPressureIntegrationTest {
     }
 
     @Test
+    @DirtiesContext
     void getBloodPressuresTest_whenDBHasData_thenReturnListOfBloodPressure() throws Exception {
         // GIVEN
         Instant createdTime1 = Instant.parse("2024-12-04T10:00:00Z");
@@ -79,6 +81,7 @@ class BloodPressureIntegrationTest {
     }
 
     @Test
+    @DirtiesContext
     void saveBloodPressureTest_whenPayloadIsCorrect_thenReturnBloodPressureEntity() throws Exception {
         // GIVEN
         // WHEN
@@ -98,5 +101,80 @@ class BloodPressureIntegrationTest {
                 .andExpect(jsonPath("$.systolic").value(120))
                 .andExpect(jsonPath("$.diastolic").value(80))
                 .andExpect(jsonPath("$.createdTime").value("2024-12-10T10:00:00Z"));
+    }
+
+    @Test
+    @DirtiesContext
+    void saveBloodPressureTest_whenPayloadIsNotCorrect_thenThrow() throws Exception {
+        // GIVEN
+        // WHEN
+        mockMvc.perform(post("/api/blood-pressure")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                            {
+                                "systolic": 120,
+                                "diastolic": null
+                            }
+                        """)
+        )
+                // THEN
+                .andExpect(status().isInternalServerError())
+                .andExpect(content().json("""
+                    {
+                        "status": 500,
+                        "message": "The input of blood pressure is not completed."
+                    }
+                """))
+                .andExpect(jsonPath("$.timestamp").exists());
+    }
+
+    @Test
+    @DirtiesContext
+    void deleteBloodPressureTest_whenIdExists_thenDeleteBloodPressureEntity() throws Exception {
+        // GIVEN
+        Instant createdTime1 = Instant.parse("2024-12-04T10:00:00Z");
+        Instant createdTime2 = Instant.parse("2024-12-05T12:30:00Z");
+        bloodPressureRepository.save(new BloodPressure(1, 1, 120, 80, createdTime1));
+        bloodPressureRepository.save(new BloodPressure(2, 1, 130, 75, createdTime2));
+
+        mockMvc.perform(get("/api/blood-pressure"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                    [
+                        {
+                            "bloodPressureId": 1,
+                            "userId": 1,
+                            "systolic": 120,
+                            "diastolic": 80,
+                            "createdTime": "2024-12-04T10:00:00Z"
+                        },
+                        {
+                            "bloodPressureId": 2,
+                            "userId": 1,
+                            "systolic": 130,
+                            "diastolic": 75,
+                            "createdTime": "2024-12-05T12:30:00Z"
+                        }
+                    ]
+                """));
+
+        // WHEN
+        mockMvc.perform(delete("/api/blood-pressure/1"))
+                // THEN
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/api/blood-pressure"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                    [
+                        {
+                          "bloodPressureId": 2,
+                          "userId": 1,
+                          "systolic": 130,
+                          "diastolic": 75,
+                          "createdTime": "2024-12-05T12:30:00Z"
+                        }
+                    ]
+                """));
     }
 }
